64b398449da7637ce600e7c0c23a77b3bdb86206
Dogukan <dokii2003@gmail.com>
2022-03-24 20:29:21 +0100

    Verbindung zwischen Raspi

--- uv_1.py
+++ uv_1.py
@@ -1,6 +1,9 @@
 import board
 import adafruit_veml6070
+import time
+import serial
 
+ser = serial.Serial ("/dev/ttyS0", 9600)
 
 with board.I2C() as i2c:
     uv = adafruit_veml6070.VEML6070(i2c)
@@ -10,7 +13,14 @@ with board.I2C() as i2c:
         risk_level = uv.get_index(uv_raw)
         uv_zahl=uv_raw
         print("Reading: {0} | Risk Level: {1}".format(uv_raw, risk_level))
-        
+        ser.write(str.encode(str(uv_raw)))
+        time.sleep(1)
+        received_data = ser.read()              
+        sleep(0.03)
+        data_left = ser.inWaiting()             
+        received_data += ser.read(data_left)                  
+        uv2_zahl=int(received_data)
+        print("UV:"uv2_zahl)
         
         
         
