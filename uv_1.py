9ca510dd465c97f7c993423252eae25902213b60
Dogukan <dokii2003@gmail.com>
2022-03-24 21:50:53 +0100

    fix

--- uv_1.py
+++ uv_1.py
@@ -24,7 +24,6 @@ with board.I2C() as i2c:
         risk_level = uv.get_index(uv_raw)
         uv_zahl=uv_raw
         print("Reading: {0} | Risk Level: {1}".format(uv_raw, risk_level))
-        ser.write(str.encode(str(uv_raw)))
         time.sleep(1)
         received_data = ser.read()              
         sleep(0.03)
